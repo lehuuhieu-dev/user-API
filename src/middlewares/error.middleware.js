4f4a70adca2e51f1f8a3a539c04eb39eedc21855
lehuuhieu0805 <01643815296h@gmail.com>
2024-08-07 22:56:31 +0700

    fix: get statusCode in middleware handle exception

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ import { BaseResponse } from '../common/responses/base.response.js';
 import { STATUS_CODE } from '../constants/index.js';
 
 const errorHandler = (error, req, res, next) => {
-    const statusCode = error.status ?? STATUS_CODE.SERVER_ERROR;
+    const statusCode = error.statusCode ?? STATUS_CODE.SERVER_ERROR;
     const errorMessage = error.message ?? 'Something went wrong, try again later';
 
     return new BaseResponse({
